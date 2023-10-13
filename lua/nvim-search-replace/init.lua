4c3e376b5b869b4d97a8f33fdd44c176baccae1d
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-10-13 10:16:14 +0200

    ReplaceAll -> Replace
    uses an interactive replace
    also resets the cursor position

--- lua/nvim-search-replace/init.lua
+++ lua/nvim-search-replace/init.lua
@@ -5,39 +5,38 @@ local function get_selection()
   local _, line_end, column_end = unpack(vim.fn.getpos("'>"))
   if line_start ~= line_end then
     print("mulit line selection not supported")
-    return "", false
+    return "", line_start, column_start, false
   end
   local selection = vim.api.nvim_buf_get_text(0, line_start - 1, column_start - 1, line_end - 1, column_end, {})[1]
-  return selection, true
-end
-
-local function termcodes(str)
-  return vim.api.nvim_replace_termcodes(str, true, false, true)
-end
-
-local function feedkeys(str)
-  vim.api.nvim_feedkeys(str, "m", false)
+  return selection, line_start, column_start, true
 end
 
 local function replace_all()
-  local selection, success = get_selection()
+  local selection, line_start, column_start, success = get_selection()
   if not success then
+    vim.fn.cursor(line_start, column_start)
     return
   end
   local escape_characters = "\"\\/.*$^~[]"
   local escaped_selection = vim.fn.escape(selection, escape_characters)
-
-  -- At first a search will be executed.
-  -- This is only for highlighting purposes.
-  feedkeys("/" .. escaped_selection .. termcodes("<CR>") .. "N")
-  feedkeys(":%s/" .. escaped_selection .. "//g" .. termcodes("<Left><Left>"))
+  vim.cmd("/" .. escaped_selection)
+  vim.cmd("redraw")
+  local replacement = vim.fn.input({prompt = "Replace: ", cancelreturn = -1})
+  if replacement == -1 then
+    vim.fn.cursor(line_start, column_start)
+    vim.cmd("noh")
+    return
+  end
+  vim.cmd("%s/" .. escaped_selection .. "/" .. replacement .. "/gc")
+  vim.fn.cursor(line_start, column_start)
+  vim.cmd("noh")
 end
 
 local function setup()
    vim.api.nvim_create_user_command(
-    "ReplaceAll",
+    "Replace",
     replace_all,
-    { nargs = 0, desc = "Replace all occurrences of selected text", range = true }
+    { nargs = 0, desc = "Replace occurrences of selected text", range = true }
   )
 end
 
